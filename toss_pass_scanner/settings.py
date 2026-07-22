9e55ed1fef491840c717e8013405ba10601556d4
syedjaferk <learnwithjafer@gmail.com>
2026-07-22 10:43:09 +0530

    debug true

--- toss_pass_scanner/settings.py
+++ toss_pass_scanner/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'tossconf26-dev-secret-change-in-prod')
 
-DEBUG = 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
